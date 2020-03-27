7c3d2724cdb28cd82ba9be5cb5ce5d9c2aec955a
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-27 15:27:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -121,7 +121,7 @@ class Student
     sql = <<-SQL 
       SELECT * 
       FROM students 
-      WHERE grade = 10 
+      WHERE grade = ? 
       SQL
       DB[:conn].execute(sql).map do |row|
       self.new_from_db(row) 
