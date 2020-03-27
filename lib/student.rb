03bddabb7dd10ba2a17d0c72c71f1f0316e27f9a
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-27 15:15:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -99,7 +99,7 @@ class Student
       SELECT * 
       FROM students 
       WHERE grade = 10 
-      LIMIT = num
+      LIMIT = ?
       SQL
       DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
