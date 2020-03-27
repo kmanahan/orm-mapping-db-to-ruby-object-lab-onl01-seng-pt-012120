605a41d1685c48e01c3fc2de905d9b387546e65b
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-27 14:34:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,14 +94,14 @@ class Student
     end
   end 
   
-  def self.first_X_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10
      sql = <<-SQL 
-      SELECT x
+      SELECT *
       FROM students 
       WHERE grade = 10 
       SQL
-      DB[:conn].execute(sql, x).map do |row|
+      DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end
+    end.first
   end 
 end
