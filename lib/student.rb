acd278a07b3853b30bb8fad19de992f1803ccd22
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-27 14:17:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,15 +94,14 @@ class Student
     end
   end 
   
-  def self.first_X_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10
      sql = <<-SQL 
-      SELECT TOP(x)
+      SELECT TOP()
       FROM students 
-      ORDER BY x ASC
       WHERE grade = 10 
       SQL
-      DB[:conn].execute(sql, x).map do |row|
+      DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end 
 end
