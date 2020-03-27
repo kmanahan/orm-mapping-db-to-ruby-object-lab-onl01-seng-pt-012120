084a658a931e9951d2c25dbe58db563d7248e53c
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-27 14:08:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -96,7 +96,7 @@ class Student
   
   def self.first_X_students_in_grade_10
      sql = <<-SQL 
-      SELECT TOP 1
+      SELECT FIRST(students)
       FROM students
       WHERE grade = 10
       SQL
