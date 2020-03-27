6c45590d4c254e54de7c4b59155f4d7d7fcb5536
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-27 14:33:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,13 +94,13 @@ class Student
     end
   end 
   
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(x)
      sql = <<-SQL 
-      SELECT *
+      SELECT COUNT(x)
       FROM students 
       WHERE grade = 10 
       SQL
-      DB[:conn].execute(sql).map do |row|
+      DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end 
